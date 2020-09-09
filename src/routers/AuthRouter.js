e2337496a601bcf4b05491b53b7d002698a8c3dc
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-09-08 20:19:18 -0500

    Estilos de Auth

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { LoginScreen } from '../components/auth/LoginScreen'
-import { RegisterScreen } from '../components/auth/RegisterScreen'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import React from "react";
+import { LoginScreen } from "../components/auth/LoginScreen";
+import { RegisterScreen } from "../components/auth/RegisterScreen";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 export const AuthRouter = () => {
-    return (
-        <>
-        <div>
-          <Switch>
-            <Route path="/auth/login" component={ LoginScreen } />
-            <Route path="/auth/register" component={ RegisterScreen } />
-            <Redirect to="/auth/login"/>
-          </Switch>
-        </div>
-      </>
-    )
-}
+  return (
+    <div className="auth__main">
+      <div className="auth__box-container">
+        <Switch>
+          <Route path="/auth/login" component={LoginScreen} />
+          <Route path="/auth/register" component={RegisterScreen} />
+          <Redirect to="/auth/login" />
+        </Switch>
+      </div>
+    </div>
+  );
+};
